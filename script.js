1cd1f77553addf57b0d86c4383563a68c6b426a0
Kelsy Watkins <klwatkins132@gmail.com>
2022-02-02 15:59:56 -0700

    add multiple modal functionality

--- script.js
+++ script.js
@@ -15,8 +15,26 @@ const closeModal = function () {
     overlay.classList.add('hidden');
 };
 
+const changeModalContent = function (event) {
+    const id = event.target.id;
+    let content = '';
+    if (id === 'modal2') {
+        content = `Duis aute irure dolor in reprehenderit in voluptat
+      velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.`
+    } else {
+        content = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.`
+    }
+    document.getElementById("modal-content").innerHTML = content
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener('click', openModal)
+    btnsOpenModal[i].addEventListener('click', changeModalContent)
 };
 
 btnCloseModal.addEventListener('click', closeModal);
