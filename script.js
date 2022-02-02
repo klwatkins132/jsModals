ab4b6827accc79842053bc0247dd26c1ebac474e
Kelsy Watkins <klwatkins132@gmail.com>
2022-02-02 15:40:24 -0700

    Add event listener for escape key

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-
 const openModal = function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -22,3 +21,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
